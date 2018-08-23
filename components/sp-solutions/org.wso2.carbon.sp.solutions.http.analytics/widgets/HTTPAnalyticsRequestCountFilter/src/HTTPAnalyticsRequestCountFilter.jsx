f2ee8bcb96f1b3d90fb2f5c9d8f60eb045ffedc7
ruwiniwj <ruwi1025@gmail.com>
2018-08-23 20:46:11 +0530

    Fix autocomplete dropdown

--- components/sp-solutions/org.wso2.carbon.sp.solutions.http.analytics/widgets/HTTPAnalyticsRequestCountFilter/src/HTTPAnalyticsRequestCountFilter.jsx
+++ components/sp-solutions/org.wso2.carbon.sp.solutions.http.analytics/widgets/HTTPAnalyticsRequestCountFilter/src/HTTPAnalyticsRequestCountFilter.jsx
@@ -20,26 +20,25 @@ import Widget from '@wso2-dashboards/widget';
 import {MuiThemeProvider, createMuiTheme} from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import MenuItem from '@material-ui/core/MenuItem';
-import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
-import CancelIcon from '@material-ui/icons/Cancel';
-import ArrowDropUpIcon from '@material-ui/icons/ArrowDropUp';
-import ClearIcon from '@material-ui/icons/Clear';
 import Chip from '@material-ui/core/Chip';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Popper from '@material-ui/core/Popper';
+import Paper from '@material-ui/core/Paper';
 import Select from 'react-select';
 import JssProvider from 'react-jss/lib/JssProvider';
-import {Scrollbars} from "react-custom-scrollbars";
+import {Scrollbars} from 'react-custom-scrollbars';
 
 const darkTheme = createMuiTheme({
     palette: {
-        type: "dark"
+        type: 'dark'
     }
 });
 
 const lightTheme = createMuiTheme({
     palette: {
-        type: "light"
+        type: 'light'
     }
 });
 
@@ -94,10 +93,9 @@ const customStyles = {
                 : isFocused ? 'rgba(255, 255, 255, 0.1)' : null,
         };
     },
-
     menuList: () => ({
         backgroundColor: 'rgb(51, 51, 51)',
-    }),
+    })
 };
 
 const allOption = [{
@@ -106,72 +104,147 @@ const allOption = [{
     disabled: false
 }];
 
-/**
- * Options class passed to the react-select component
- */
-class Option extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+let openPopper = false;
 
-    render() {
-        const {children, isFocused, onFocus, isDisabled} = this.props;
-        return (
-            <MenuItem
-                onFocus={onFocus}
-                selected={isFocused}
-                disabled={isDisabled}
-                onClick={() => this.props.onSelect(this.props.option, event)}
-                component="div"
-            >
-                {children}
-            </MenuItem>
-        );
-    }
-}
-
-/**
- * Function to wrap react-select component
- * @param props
- * @returns <Select> componet
- * @constructor
- */
-function SelectWrapped(props) {
-    const {classes, isClearable, ...other} = props;
+function NoOptionsMessage(props) {
     return (
-        <Select
-            styles={props.muiTheme.name === 'dark' ? customStyles : {}}
-            optionComponent={Option}
-            noResultsText={<div>{'No results found'}</div>}
-            arrowRenderer={arrowProps => {
-                return arrowProps.isOpen ? <ArrowDropUpIcon/> : <ArrowDropDownIcon/>;
+        <Typography
+            style={{
+                padding: 15,
+                color: '#7e7e7e'
             }}
-            isClearable={isClearable}
-            clearRenderer={() => <ClearIcon/>}
-            valueComponent={valueProps => {
-                const {value, children, onRemove} = valueProps;
-                const onDelete = event => {
-                    event.preventDefault();
-                    event.stopPropagation();
-                    onRemove(value);
-                };
-                if (onRemove) {
-                    return (
-                        <Chip
-                            tabIndex={-1}
-                            label={children}
-                            className={classes.chip}
-                            deleteIcon={<CancelIcon onTouchEnd={onDelete}/>}
-                            onDelete={onDelete}
-                        />
-                    );
-                }
-                return <div className="Select-value">{children}</div>;
-            }}
-            {...other}
-        />
+            {...props.innerProps}>
+            {props.children}
+        </Typography>
     );
 }
+
+function inputComponent({inputRef, ...props}) {
+    return <div
+        ref={inputRef}
+        {...props}/>;
+}
+
+function Control(props) {
+    openPopper = props.selectProps.menuIsOpen;
+    return (
+        <TextField
+            id='popperAnchorTextField'
+            style={{display: 'flex'}}
+            fullWidth={true}
+            InputProps={{
+                inputComponent,
+                inputProps: {
+                    inputRef: props.innerRef,
+                    children: props.children,
+                    ...props.innerProps,
+                },
+            }}
+            {...props.selectProps.textFieldProps}/>
+    );
+}
+
+function Option(props) {
+    return (
+        <MenuItem
+            buttonRef={props.innerRef}
+            selected={props.isFocused}
+            component='div'
+            style={{
+                fontWeight: props.isSelected ? 500 : 400,
+            }}
+            {...props.innerProps}>
+            {props.children}
+        </MenuItem>
+    );
+}
+
+function Placeholder(props) {
+    return (
+        <Typography
+            style={{
+                position: 'absolute',
+                left: 2,
+                fontSize: '90%',
+                color: '#7e7e7e'
+            }}
+            {...props.innerProps}>
+            {props.children}
+        </Typography>
+    );
+}
+
+function SingleValue(props) {
+    return (
+        <Typography {...props.innerProps}>
+            {props.children}
+        </Typography>
+    );
+}
+
+function ValueContainer(props) {
+    return (
+        <div
+            style={{
+                display: 'flex',
+                flexWrap: 'wrap',
+                flex: 1,
+                alignItems: 'center',
+            }}>
+            {props.children}
+        </div>);
+}
+
+function MultiValue(props) {
+    return (
+        <Chip
+            tabIndex={-1}
+            label={props.children}
+            onDelete={event => {
+                props.removeProps.onClick();
+                props.removeProps.onMouseDown(event);
+            }}
+            style={{
+                borderRadius: 15,
+                display: 'flex',
+                flexWrap: 'wrap',
+                fontSize: '90%',
+                overflow: 'hidden',
+                paddingLeft: 6,
+                textOverflow: 'ellipsis',
+                whiteSpace: 'nowrap',
+                minWidth: '20',
+                margin: 2
+            }}/>
+    );
+}
+
+function Menu(props) {
+    let popperNode = document.getElementById('popperAnchorTextField');
+    return (
+        <Popper
+            open={openPopper}
+            anchorEl={popperNode}>
+            <Paper
+                square
+                style={{width: popperNode ? popperNode.clientWidth : null}}
+                {...props.innerProps}>
+                {props.children}
+            </Paper>
+        </Popper>
+    );
+}
+
+const components = {
+    Option,
+    Control,
+    NoOptionsMessage,
+    Placeholder,
+    SingleValue,
+    MultiValue,
+    ValueContainer,
+    Menu,
+};
 
 /**
  * HTTPAnalyticsRequestCountFilter which renders the perspective and filter in home page
@@ -268,18 +341,18 @@ class HTTPAnalyticsRequestCountFilter extends Widget {
 
         if (name === 0) {
             options = this.state.services;
-            selectedOptionLabelName = "selectedServiceValues";
-            selectedOptionsName = "serviceOptions";
+            selectedOptionLabelName = 'selectedServiceValues';
+            selectedOptionsName = 'serviceOptions';
             selectedValues = values;
         } else if (name === 1) {
             options = this.state.servers;
-            selectedOptionLabelName = "selectedServerValues";
-            selectedOptionsName = "serverOptions";
+            selectedOptionLabelName = 'selectedServerValues';
+            selectedOptionsName = 'serverOptions';
             selectedValues = values;
         } else {
             options = this.state.services;
-            selectedOptionLabelName = "selectedSingleServiceValue";
-            selectedOptionsName = "serviceOptions";
+            selectedOptionLabelName = 'selectedSingleServiceValue';
+            selectedOptionsName = 'serviceOptions';
             selectedValues = new Array(1);
             selectedValues[0] = values;
         }
@@ -332,88 +405,86 @@ class HTTPAnalyticsRequestCountFilter extends Widget {
     render() {
         const {classes} = this.props;
         return (
-            <JssProvider generateClassName={generateClassName}>
-                <MuiThemeProvider theme={this.props.muiTheme.name === 'dark' ? darkTheme : lightTheme}>
-                    <Scrollbars style={{height: this.state.height}}>
-                        <div style={{paddingLeft: 24, paddingRight: 16}}>
+            <JssProvider
+                generateClassName={generateClassName}>
+                <MuiThemeProvider
+                    theme={this.props.muiTheme.name === 'dark' ? darkTheme : lightTheme}>
+                    <Scrollbars
+                        style={{height: this.state.height}}>
+                        <div
+                            style={{
+                                paddingLeft: 24,
+                                paddingRight: 16
+                            }}>
                             <Tabs
                                 value={this.state.perspective}
                                 onChange={(evt, value) => this.setState({perspective: value}, this.publishUpdate)}>
-                                <Tab label="Server"/>
-                                <Tab label="Service"/>
-                                <Tab label="Method"/>
+                                <Tab label='Service'/>
+                                <Tab label='Server'/>
+                                <Tab label='Method'/>
                             </Tabs>
-                            <div style={{paddingLeft: 10, paddingRight: 16, paddingTop: 3}}>
+                            <div
+                                style={{paddingLeft: 10, paddingRight: 16, paddingTop: 3}}>
                                 {
                                     this.state.perspective === 0 &&
-                                    <TextField
-                                        fullWidth={true}
+                                    <Select
+                                        className='autocomplete'
+                                        classNamePrefix='autocomplete'
+                                        styles={this.props.muiTheme.name === 'dark' ? customStyles : {}}
+                                        classes={classes}
+                                        textFieldProps={{
+                                            label: '',
+                                            InputLabelProps: {
+                                                shrink: false,
+                                            },
+                                        }}
+                                        options={this.state.serviceOptions}
+                                        components={components}
                                         value={this.state.selectedServiceValues}
                                         onChange={this.handleChange(0)}
-                                        placeholder="Filter by Service"
-                                        label=""
-                                        InputLabelProps={{
-                                            shrink: false,
-                                        }}
-                                        InputProps={{
-                                            inputComponent: SelectWrapped,
-                                            inputProps: {
-                                                classes,
-                                                isMulti: true,
-                                                simpleValue: true,
-                                                options: this.state.serviceOptions,
-                                                muiTheme: this.props.muiTheme,
-                                                isClearable: true
-                                            }
-                                        }}
+                                        placeholder='Filter by Service'
+                                        isMulti
                                     />
                                 }
                                 {
                                     this.state.perspective === 1 &&
-                                    <TextField
-                                        fullWidth={true}
+                                    <Select
+                                        className='autocomplete'
+                                        classNamePrefix='autocomplete'
+                                        styles={this.props.muiTheme.name === 'dark' ? customStyles : {}}
+                                        classes={classes}
+                                        textFieldProps={{
+                                            label: '',
+                                            InputLabelProps: {
+                                                shrink: false,
+                                            },
+                                        }}
+                                        options={this.state.serverOptions}
+                                        components={components}
                                         value={this.state.selectedServerValues}
                                         onChange={this.handleChange(1)}
-                                        placeholder="Filter by Server"
-                                        label=""
-                                        InputLabelProps={{
-                                            shrink: false,
-                                        }}
-                                        InputProps={{
-                                            inputComponent: SelectWrapped,
-                                            inputProps: {
-                                                classes,
-                                                isMulti: true,
-                                                simpleValue: true,
-                                                options: this.state.serverOptions,
-                                                muiTheme: this.props.muiTheme,
-                                                isClearable: true
-                                            }
-                                        }}
+                                        placeholder='Filter by Server'
+                                        isMulti
                                     />
                                 }
                                 {
                                     this.state.perspective === 2 &&
-                                    <TextField
-                                        fullWidth={true}
+                                    <Select
+                                        className='autocomplete'
+                                        classNamePrefix='autocomplete'
+                                        styles={this.props.muiTheme.name === 'dark' ? customStyles : {}}
+                                        classes={classes}
+                                        textFieldProps={{
+                                            label: '',
+                                            InputLabelProps: {
+                                                shrink: false,
+                                            },
+                                        }}
+                                        options={this.state.serviceOptions}
+                                        components={components}
                                         value={this.state.selectedSingleServiceValue}
                                         onChange={this.handleChange(2)}
-                                        placeholder="Choose a Service"
-                                        label=""
-                                        InputLabelProps={{
-                                            shrink: false,
-                                        }}
-                                        InputProps={{
-                                            inputComponent: SelectWrapped,
-                                            inputProps: {
-                                                classes,
-                                                isMulti: false,
-                                                simpleValue: true,
-                                                options: this.state.serviceOptions,
-                                                muiTheme: this.props.muiTheme,
-                                                isClearable: false
-                                            }
-                                        }}
+                                        placeholder='Choose a Service'
                                     />
                                 }
                             </div>
@@ -444,7 +515,7 @@ export const generateClassName = (rule, styleSheet) => {
         if (prefix.match(/^Mui/)) {
             return `${prefix}-${rule.key}`;
         }
-
+        
         return `${prefix}-${rule.key}-${classCounter}`;
     }
 
@@ -455,4 +526,4 @@ export const generateClassName = (rule, styleSheet) => {
 HTTPAnalyticsRequestCountFilter.propTypes = {
     classes: PropTypes.object.isRequired,
 };
-global.dashboard.registerWidget("HTTPAnalyticsRequestCountFilter", HTTPAnalyticsRequestCountFilter);
+global.dashboard.registerWidget('HTTPAnalyticsRequestCountFilter', HTTPAnalyticsRequestCountFilter);
