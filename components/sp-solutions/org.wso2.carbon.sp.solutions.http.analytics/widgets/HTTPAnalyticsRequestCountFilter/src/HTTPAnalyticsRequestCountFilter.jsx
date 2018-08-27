2c9da1cf5c7c802cead053fa6171f27ba2ae3955
ruwiniwj <ruwi1025@gmail.com>
2018-08-27 16:33:02 +0530

    Remove custom styles

--- components/sp-solutions/org.wso2.carbon.sp.solutions.http.analytics/widgets/HTTPAnalyticsRequestCountFilter/src/HTTPAnalyticsRequestCountFilter.jsx
+++ components/sp-solutions/org.wso2.carbon.sp.solutions.http.analytics/widgets/HTTPAnalyticsRequestCountFilter/src/HTTPAnalyticsRequestCountFilter.jsx
@@ -42,62 +42,6 @@ const lightTheme = createMuiTheme({
     }
 });
 
-const customStyles = {
-    input: () => ({
-        color: 'white'
-    }),
-    multiValue: () => ({
-        borderRadius: 15,
-        display: 'flex',
-        flexWrap: 'wrap',
-        color: 'black',
-        fontSize: '90%',
-        overflow: 'hidden',
-        paddingLeft: 6,
-        textOverflow: 'ellipsis',
-        whiteSpace: 'nowrap',
-        backgroundColor: 'darkgrey',
-        minWidth: '20'
-    }),
-    singleValue: () => ({
-        display: 'flex',
-        flexWrap: 'wrap',
-        color: 'white',
-        fontSize: '95%',
-    }),
-    control: () => ({
-        height: 10,
-        borderRadius: 5,
-        alignItems: 'center',
-        minHeight: 30,
-        backgroundColor: 'rgb(51, 51, 51)',
-        borderColor: 'grey',
-        borderStyle: 'solid',
-        borderWidth: 0,
-        boxShadow: '0 0 0 1px grey',
-        cursor: 'default',
-        display: 'flex',
-        flexWrap: 'wrap',
-        justifyContent: 'space-between',
-        outline: '0 !important',
-        position: 'relative',
-        transition: 'all 100ms',
-        paddingTop: 2
-    }),
-    option: (styles, {data, isDisabled, isFocused}) => {
-        return {
-            ...styles,
-            height: 30,
-            backgroundColor: isDisabled
-                ? null
-                : isFocused ? 'rgba(255, 255, 255, 0.1)' : null,
-        };
-    },
-    menuList: () => ({
-        backgroundColor: 'rgb(51, 51, 51)',
-    })
-};
-
 const allOption = [{
     value: 'All',
     label: 'All',
@@ -150,9 +94,7 @@ function Option(props) {
             buttonRef={props.innerRef}
             selected={props.isFocused}
             component='div'
-            style={{
-                fontWeight: props.isSelected ? 500 : 400,
-            }}
+            style={{fontWeight: props.isSelected ? 500 : 400}}
             {...props.innerProps}>
             {props.children}
         </MenuItem>
@@ -176,7 +118,14 @@ function Placeholder(props) {
 
 function SingleValue(props) {
     return (
-        <Typography {...props.innerProps}>
+        <Typography
+            style={{
+                display: 'flex',
+                flexWrap: 'wrap',
+                color: 'white',
+                fontSize: '95%'
+            }}
+            {...props.innerProps}>
             {props.children}
         </Typography>
     );
@@ -229,12 +178,7 @@ function Menu(props) {
                 square
                 style={{width: popperNode ? popperNode.clientWidth : null}}
                 {...props.innerProps}>
-                <Scrollbars
-                    autoHeight
-                    autoHeightMin={0}
-                    autoHeightMax={250}>
-                    {props.children}
-                </Scrollbars>
+                {props.children}
             </Paper>
         </Popper>
     );
@@ -433,10 +377,9 @@ class HTTPAnalyticsRequestCountFilter extends Widget {
                                 {
                                     this.state.perspective === 0 &&
                                     <Select
+                                        classes={classes}
                                         className='autocomplete'
                                         classNamePrefix='autocomplete'
-                                        styles={this.props.muiTheme.name === 'dark' ? customStyles : {}}
-                                        classes={classes}
                                         textFieldProps={{
                                             label: '',
                                             InputLabelProps: {
@@ -454,10 +397,9 @@ class HTTPAnalyticsRequestCountFilter extends Widget {
                                 {
                                     this.state.perspective === 1 &&
                                     <Select
+                                        classes={classes}
                                         className='autocomplete'
                                         classNamePrefix='autocomplete'
-                                        styles={this.props.muiTheme.name === 'dark' ? customStyles : {}}
-                                        classes={classes}
                                         textFieldProps={{
                                             label: '',
                                             InputLabelProps: {
@@ -475,10 +417,9 @@ class HTTPAnalyticsRequestCountFilter extends Widget {
                                 {
                                     this.state.perspective === 2 &&
                                     <Select
+                                        classes={classes}
                                         className='autocomplete'
                                         classNamePrefix='autocomplete'
-                                        styles={this.props.muiTheme.name === 'dark' ? customStyles : {}}
-                                        classes={classes}
                                         textFieldProps={{
                                             label: '',
                                             InputLabelProps: {
